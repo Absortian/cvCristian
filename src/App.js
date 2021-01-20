9965abb4c28512e8419936b54628dfce06cae525
clopez <clopez@aszende.com>
2021-01-20 10:39:55 +0100

    Fix

--- src/App.js
+++ src/App.js
@@ -15,12 +15,13 @@ class App extends Component {
       bio: 'Persona curiosa y proactiva. Desarrollador Full-Stack combinado con características DevOps y experiencia de Team Lead.',
       address: 'Badalona, Barcelona',
       social: [
-        {name: 'envelope', url: 'mailto:lopez.cristian.deljulia@gmail.com'},
-        {name: 'twitter', url: 'https://twitter.com/kr00nox'},
+        {name: 'envelope', url: 'mailto:absortian@gmail.com'},
+        {name: 'twitter', url: 'https://twitter.com/absortian'},
         {name: 'github', url: 'https://github.com/absortian'},
         {name: 'linkedin', url: 'https://www.linkedin.com/in/cristian-lópez-márquez-324743117/'}
       ],
       experience: [
+        {jobTitle: 'Web Developer', company: 'Aszende', startDate: 'Abr 2020', endDate: 'Actualidad', jobDescription: '<p>Desarrollo de aplicaciones internas.</p><p>Creación desde cero de sistema de gestión de flota de vehículos basado en API Rest con múltiples integraciones.</p><p>Integración de aplicaciones web con SAP</p><p>Desarrollo PHP con framework Symfony en versiones 2.8, 4.4 y 5.1</p>'},
         {jobTitle: 'Full Stack Developer / Team Lead Developer', company: 'Eurotransportcar (Kosaas)', startDate: 'Feb 2018', endDate: 'Abr 2020', jobDescription: "<ul><li>Desarrollo Full Stack.</li><li>Framework Symfony en diferentes proyectos con versiones:<ul><li>2.3</li><li>2.8</li><li>3.4</li><li>4.4</li></ul></li><li>Uso de Docker con orquestador Kubernetes.</li><li>Uso de control de versiones GIT con metodología GitFlow.</li><li>Desarrollo basado en sprint con el uso de Jira Software.</li><li>Realización de reuniones de formación interna / gestión de nuevos desarrollos con clientes.</li><li>Subidas de código périodicas, mínimo una semanal.</li><li>Lead de grupos de desarrollo</li><li>SysAdmin de entorno de preproducción y producción de Linux + Docker + Kubernetes.</li></ul>"},
         {jobTitle: 'SysAdmin', company: 'Birchman Group Spain', startDate: 'Oct 2016', endDate: 'Feb 2018', jobDescription: 'Administración de sistemas en CPDs que hospedaban máquinas virtuales bajo VMWare. Administración Linux y Windows.'},
       ],
@@ -34,16 +35,18 @@ class App extends Component {
         {name: 'CCNA Discovery', institution: 'Cisco', date: '2014', description: 'Certificación de Redes' },
       ],
       skills: [
+        {name: 'WebServices', percentage: '90%'},
+        {name: 'API Rest', percentage: '90%'},
         {name: 'Git', percentage: '100%'},
         {name: 'Symfony', percentage: '90%'},
         {name: 'PHP', percentage: '90%'},
-        {name: 'HTML5', percentage: '95%'},
-        {name: 'CSS', percentage: '90%'},
+        {name: 'HTML5', percentage: '80%'},
+        {name: 'CSS', percentage: '70%'},
         {name: 'JavaScript', percentage: '75%'},
         {name: 'SQL', percentage: '90%'},
         {name: 'Linux', percentage: '90%'},
         {name: 'Windows', percentage: '90%'},
-        {name: 'Networking', percentage: '90%'},
+        {name: 'Networking', percentage: '80%'},
         {name: 'Docker', percentage: '90%'},
         {name: 'Kubernetes', percentage: '90%'},
       ]
